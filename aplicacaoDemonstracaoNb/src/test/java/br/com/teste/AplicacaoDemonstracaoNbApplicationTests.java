a6b8226e81b90cf80ab228f829296ff766a8f390
FernandoPucci <fsilvapucci@gmail.com>
2016-11-28 14:36:17 -0200

    .

--- aplicacaoDemonstracaoNb/src/test/java/br/com/teste/AplicacaoDemonstracaoNbApplicationTests.java
+++ aplicacaoDemonstracaoNb/src/test/java/br/com/teste/AplicacaoDemonstracaoNbApplicationTests.java
@@ -1,8 +1,13 @@
 package br.com.teste;
 
 
+import br.com.teste.dao.AlunoDAO;
+import br.com.teste.model.Aluno;
+import java.util.Date;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,10 +16,24 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class AplicacaoDemonstracaoNbApplicationTests {
 
+    @Autowired
+    AlunoDAO alunoDAO;
     
     
 	@Test
 	public void contextLoads() {
+            new SpringApplicationBuilder(AplicacaoDemonstracaoNbApplicationTests.class)
+                .headless(false)
+                .web(false)
+                .run();
 	}
+        
+        @Test
+        public void tester(){
+        
+            Aluno aluno = new Aluno("ZE", "silva", new Date());
+            alunoDAO.save(aluno);
+            
+        }
 
 }
